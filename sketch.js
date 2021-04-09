9f0983374b504d69d98b4b9b6d25b8f2090a79ae
kool-kavin <76016081+kool-kavin@users.noreply.github.com>
2021-04-08 20:39:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var rain
+
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    rain = new Rain(600,200,50,50);
+    
+}
+
+function draw(){
+    Engine.update(engine);
+    rectMode(CENTER);
+    rain.display();
+        
+}
+
+
+
